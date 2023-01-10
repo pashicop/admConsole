68b1d4d269ae8a63c4c98cd0c85cd06e33a6111b
Pavel shitikov <shitikov@astracom.ru>
2023-01-10 20:14:25 +0300

    bug fixes

--- scripts/SE1.7/install_se_1.7.sh
+++ scripts/SE1.7/install_se_1.7.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+PA=0
+TP=0
 printf '##### Установка сервиса ОМЕГА #####\n'
 printf '\n##### Удаление старой версии #####\n'
 . ~/.bashrc
@@ -25,8 +27,10 @@ printf '\n##### Обновляем пакеты ASTRALINUX #####'
 printf '\n##### Это может занять много времени!! #####'
 printf '\n##### Пожалуйста, подождите! #####\n'
 date
+sleep 10
+date
 sudo apt-get update >> ~/install_log.txt 2>&1 &&
-sudo apt-get -y install openssh-server >> ~/install_log.txt 2>&1 &&
+sudo apt-get -y install openssh-server xorgxrdp xrdp >> ~/install_log.txt 2>&1 &&
 sudo apt-get -y install postgresql >> ~/install_log.txt 2>&1
 if [[ $? == 0 ]]
   then printf '##### ------------OK----------- #####\n'
@@ -76,7 +80,22 @@ unset PGPASSWORD
 printf '##### ------------OK----------- #####\n'
 printf '\n##### Настройка ОМЕГИ #####\n'
 chmod +x first_run run Api
-./first_run
+echo -e "\033[31mВы хотите заполнить сервер тестовыми данными? Y/n|Д/н]:\033[0m"
+read -n 3 CONF_D
+CONF_D="${CONF_D:-Y}"
+case $CONF_D in
+  y|Y|yes|Yes|"Д"|"д"|"Да"|"да")
+    echo "Данные будут добавлены"
+    TP=1;;
+  n|N|no|No|"Н"|"н"|"Нет"|"нет")
+    echo "Тестовые данные не будут добавлены";;
+esac
+if [[ $TP -eq 1 ]]
+  then
+    ./first_run true
+  else
+    ./first_run
+fi
 printf '##### ------------OK----------- #####\n'
 printf '\n##### Устанавливаем сервис ОМЕГИ #####\n'
 sudo cp ~/Omega/omega.service /lib/systemd/system/ &&
@@ -110,45 +129,58 @@ printf '##### ------------OK----------- #####\n'
 printf '\n##### Добавляем необходимые права #####\n'
 echo 'omega ALL=(ALL) NOPASSWD: /bin/systemctl * omega' | sudo EDITOR='tee -a' visudo
 printf '##### ------------OK----------- #####\n'
-#printf '\n##### Installing packets for admin panel #####\n'
-#sudo apt-get -y install xorgxrdp xrdp >> ~/install_log.txt
-#sudo apt-get -y install curl git >> ~/install_log.txt
-#curl https://pyenv.run | bash >> ~/install_log.txt 1>&2
-#echo '# omega start' >> ~/.bashrc
-#echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
-#echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-#echo 'eval "$(pyenv init -)"' >> ~/.bashrc
-#echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
-#echo '# omega end' >> ~/.bashrc
-##tail -n 10 ~/.bashrc
-#export PYENV_ROOT="$HOME/.pyenv"
-##echo $PYENV_ROOT
-#command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-#eval "$(pyenv init -)"
-#eval "$(pyenv virtualenv-init -)"
-#printf '\n##### Restart bash #####\n'
-##exec "$SHELL"
-#. ~/.bashrc
-#printf '\n##### Installing packets for python #####\n'
-#sudo apt-get -y install make build-essential libssl-dev zlib1g-dev \
-#libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
-#libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev >> ~/install_log.txt
-#printf '\n##### All packets have been installed #####\n'
-#printf '\n##### Installing python #####\n'
-#pyenv install 3.9.10
-#printf '\n##### Python have been installed #####\n'
-#pyenv global 3.9.10
-#python --version
-##cd ~
-##unzip ~/admConsole-tray.zip >> ~/install_log.txt
-#cd ~/admConsole/
-#pip install -r requirements.txt >> ~/install_log.txt
-#cd ~
-#mv ~/admConsole/run.sh ~/admConsole/shortcut.desktop ~/Desktop/
-#chmod +x ~/Desktop/run.sh
-#date
-#printf '\n##### Add rights to start/stop OMEGA #####\n'
-#echo 'omega ALL=(ALL) NOPASSWD: /bin/systemctl * omega' | sudo EDITOR='tee -a' visudo
-#printf '\n##### Rights added #####\n'
-#printf '\n##### Admin panel installed successfully #####\n'
-exit 0
+echo -e "\033[31mВы хотите установить панель администратора? Y/n|Д/н]:\033[0m"
+read -n 3 CONF_D
+CONF_D="${CONF_D:-Y}"
+#echo "$CONF_D"
+case $CONF_D in
+  y|Y|yes|Yes|"Д"|"д"|"Да"|"да")
+    echo "Панель будет установлена"
+    PA=1;;
+  n|N|no|No|"Н"|"н"|"Нет"|"нет")
+    echo "Панель НЕ будет установлена";;
+esac
+if [[ $PA -eq 1 ]]
+  then
+    printf '\n##### Установка панели администратора #####\n'
+    sleep 1
+    printf '\n##### Installing packets for admin panel #####\n'
+    sudo apt-get -y install xorgxrdp xrdp >> ~/install_log.txt
+    sudo apt-get -y install curl git >> ~/install_log.txt
+    curl https://pyenv.run | bash >> ~/install_log.txt 1>&2
+    echo '# omega start' >> ~/.bashrc
+    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+    echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+    echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+    echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+    echo '# omega end' >> ~/.bashrc
+    #tail -n 10 ~/.bashrc
+    export PYENV_ROOT="$HOME/.pyenv"
+    #echo $PYENV_ROOT
+    command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init -)"
+    eval "$(pyenv virtualenv-init -)"
+    printf '\n##### Restart bash #####\n'
+    #exec "$SHELL"
+    . ~/.bashrc
+    printf '\n##### Installing packets for python #####\n'
+    sudo apt-get -y install make build-essential libssl-dev zlib1g-dev \
+    libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+    libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev >> ~/install_log.txt
+    printf '\n##### All packets have been installed #####\n'
+    printf '\n##### Installing python #####\n'
+    pyenv install 3.9.10
+    printf '\n##### Python have been installed #####\n'
+    pyenv global 3.9.10
+    python --version
+    #cd ~
+    #unzip ~/admConsole-tray.zip >> ~/install_log.txt
+    cd ~/admConsole/
+    pip install -r requirements.txt >> ~/install_log.txt
+    cd ~
+  else
+    printf '\n##### Установка завершена #####\n'
+    exit 0
+fi
+
+#exit 0
