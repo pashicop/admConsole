876ec0330cd1940432a7a20df4f033355e4a0ac5
Pavel Shitikov <pashicop@gmail.com>
2024-06-10 17:25:03 +0300

    https script

--- scripts/SE1.6_2.0/install_se_1.6_3.10.sh
+++ scripts/SE1.6_2.0/install_se_1.6_3.10.sh
@@ -3,6 +3,7 @@ CR=0
 TP=0
 PA=0
 IP=0
+HTTPS=0
 
 CONF_D="Y"
 printf '##### Установка сервиса ОМЕГА #####\n'
@@ -74,6 +75,8 @@ printf '\n'
 printf '\n##### Копируем ОМЕГА ######\n'
 mv Omega/ ~/Omega
 mv admConsole/ ~/admConsole
+mv crt/ ~/crt
+mv omega.conf ~
 cd ~ && rm -R "$DIRECTORY"
 printf '##### ------------OK----------- #####\n'
 printf '\n##### Меняем настройки parsec ######\n'
@@ -110,6 +113,67 @@ unset PGPASSWORD
 printf '##### ------------OK----------- #####\n'
 printf '\n##### Настройка ОМЕГИ #####\n'
 chmod +x first_run run Api Licensing/ValidateCli Licensing
+
+echo -e "\033[31mВы хотите установить поддержку https? Y/n|Д/н]:\033[0m"
+while true
+  do
+  read -n 1 CONF_D
+  case $CONF_D in
+    y|Y|yes|Yes|"Д"|"д"|"Да"|"да")
+      printf "\nПоддержка https будет установлена\n"
+      HTTPS=1
+      break;;
+    n|N|no|No|"Н"|"н"|"Нет"|"нет")
+      printf "\nПоддержка https НЕ будет установлена\n"
+      break;;
+    *)
+      printf "\nСимвол $CONF_D не распознан - повторите!\n";;
+  esac
+done
+if [[ $HTTPS -eq 1 ]]
+  then
+    printf "\nУстанавливаем сервер Apache2\n"
+    sudo apt-get -y install apache2 >> ~/install_log.txt 2>&1
+    if [[ $? == 0 ]]
+      then printf '##### ------------OK----------- #####\n'
+      else printf '\n##### Ошибка при установке Apache2 #####\n'
+      exit 110
+    fi
+    printf "\nУстанавливаем сертификаты из папки crt\n"
+    cd ~
+    sudo cp -r crt /etc/apache2 >> ~/install_log.txt 2>&1
+    if [[ $? == 0 ]]
+      then printf '##### ------------OK----------- #####\n'
+      else printf '\n##### Ошибка при установке сертификатов #####\n'
+      exit 111
+    fi
+    printf "\nУстанавливаем файл конфигурации\n"
+    sudo cp omega.conf /etc/apache2/sites-available >> ~/install_log.txt 2>&1
+    if [[ $? == 0 ]]
+      then printf '##### ------------OK----------- #####\n'
+      else printf '\n##### Ошибка при установке файла конфигурации #####\n'
+      exit 112
+    fi
+    printf "\nКонфигурируем Apache2\n"
+#    sudo echo ServerName 127.0.0.1 >> /etc/apache2/apache2.conf &&
+#    sudo echo AstraMode off >> /etc/apache2/apache2.conf &&
+    echo -e "ServerName 127.0.0.1" | sudo tee -a /etc/apache2/apache2.conf &&
+    echo -e "AstraMode off" | sudo tee -a /etc/apache2/apache2.conf &&
+    sudo a2enmod headers &&
+    sudo a2enmod proxy &&
+    sudo a2enmod ssl &&
+    sudo a2enmod proxy_http &&
+    sudo a2enmod proxy_wstunnel &&
+    sudo a2enmod rewrite &&
+    sudo a2ensite omega &&
+    sudo apache2ctl restart
+    if [[ $? == 0 ]]
+      then printf '##### ------------OK----------- #####\n'
+      else printf '\n##### Ошибка при конфигурировании Apache2 #####\n'
+      exit 113
+    fi
+fi
+
 echo -e "\033[31mВы хотите заполнить сервер тестовыми данными? Y/n|Д/н]:\033[0m"
 while true
   do
@@ -126,6 +190,7 @@ while true
       printf "\nСимвол $CONF_D не распознан - повторите!\n";;
   esac
 done
+cd ~/Omega
 if [[ $TP -eq 1 ]]
   then
     ./first_run true
